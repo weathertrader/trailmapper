9913b6b21a2745c1bef994d5e5e88e8a4d3276bf
Your Name <you@example.com>
2020-03-29 07:43:58 -0700

    add ipynb and executable scripts

---
+++ plot_gps_points_from_geojson.py
@@ -0,0 +1,137 @@
+
+# should work from cli
+# all args in cli
+# environment.yml should work
+# functional .gitignore 
+# functional README.md 
+# myroutemap
+
+# usage
+# python plot_gps_points_from_geojson.py --data_geojson=data_geojson
+
+# imports
+import os
+import numpy as np
+import pandas as pd
+import argparse
+import numpy as np
+import matplotlib.cm as cm
+from scipy.signal import medfilt
+import argparse
+import glob
+from scipy.signal import medfilt
+
+import gpxpy
+import geopandas
+import geojson
+import folium
+from folium import plugins
+import webbrowser
+
+from utils import rgb2hex
+from utils import calc_dist_from_coords
+from utils import calc_dist_from_coordsPoint2Line
+from utils import RDP
+
+#manual_debug = True
+manual_debug = False
+if (manual_debug):
+    #dir_work = '/home/craigmatthewsmith/gps_tracks'
+    #os.chdir(dir_work)
+    data_geojson       = 'data_geojson'
+else: # parse command line parameters
+    parser = argparse.ArgumentParser(description = 'process gpx files to geojson')
+    parser.add_argument('--data_geojson',       type=str, default='data_geojson',       help = 'output .geojson file')
+    args = parser.parse_args()    
+    data_geojson       = args.data_geojson 
+
+
+# create new GeoJson objects to reduce GeoJSON data sent to Folium map as layer
+f_track = lambda x: {'color': '#FC4C02', 'weight': 5} # show some color...
+
+geojson_file_list = sorted(glob.glob(os.path.join(data_geojson, '*2017*.geojson')))
+n_files = len(geojson_file_list)
+print(n_files)                              
+
+cmap = cm.get_cmap('jet') # matplotlib colormap
+
+
+features_tracks = []
+features_elevation = []
+features_speed = []
+f = 0
+#for f in range(0, 3, 1):
+#for f in range(n_files-50, n_files, 1):
+for f in range(0, n_files, 1):
+    geojson_file = geojson_file_list[f]
+    print('  reading f %s of %s ' %(f, n_files))
+    
+    # read geojson file
+    with open(geojson_file, 'r') as file:
+        geojson_data = geojson.load(file)
+
+    #geojson_data
+
+    for feature in geojson_data['features']:
+        line = geojson.LineString(feature['geometry']['coordinates'])
+        features_tracks.append(geojson.Feature(geometry=line))
+
+    cmin_elevation = min(feature['properties']['elevation'] for feature in geojson_data['features'])
+    cmax_elevation = max(feature['properties']['elevation'] for feature in geojson_data['features'])
+    f_elevation = lambda x: {'color': rgb2hex(cmap((x['properties']['elevation']-cmin_elevation)/(cmax_elevation-cmin_elevation))), 'weight': 5} # cmap needs normalized data
+    t_elevation = folium.features.GeoJsonTooltip(fields=['elevation'], aliases=['Elevation (m)'])
+
+    for feature in geojson_data['features']:
+        line = geojson.LineString(feature['geometry']['coordinates'])
+        elevation = feature['properties']['elevation']
+        features_elevation.append(geojson.Feature(geometry=line, properties={'elevation': elevation}))
+
+    cmin_speed = min(feature['properties']['speed'] for feature in geojson_data['features'])
+    cmax_speed = max(feature['properties']['speed'] for feature in geojson_data['features'])
+    f_speed = lambda x: {'color': rgb2hex(cmap((x['properties']['speed']-cmin_speed)/(cmax_speed-cmin_speed))), 'weight': 5} # cmap needs normalized data
+    t_speed = folium.features.GeoJsonTooltip(fields=['speed'], aliases=['Speed '])
+
+    for feature in geojson_data['features']:
+        line = geojson.LineString(feature['geometry']['coordinates'])
+        speed = feature['properties']['speed']
+        features_speed.append(geojson.Feature(geometry=line, properties={'speed': speed}))
+
+print('creating map features to map ')
+
+#fmap = folium.Map(tiles='Stamen Terrain', prefer_canvas=True, disable_3d=True)
+fmap = folium.Map(tiles='Stamen Terrain', location=[37.862606, -121.978372], zoom_start=10) 
+folium.TileLayer(tiles = 'OpenStreetMap', name='OpenStreetMap', show=False).add_to(fmap)
+folium.TileLayer(tiles = 'Stamen Terrain', name='Terrain Map', show=True).add_to(fmap)
+
+print('appending features to map ')
+
+geojson_data_elevation = geojson.FeatureCollection(features_elevation)
+folium.GeoJson(geojson_data_elevation, style_function=f_elevation, tooltip=t_elevation, name='Elevation (m)', show=False, smooth_factor=3.0).add_to(fmap)
+        
+geojson_data_speed = geojson.FeatureCollection(features_speed)
+folium.GeoJson(geojson_data_speed, style_function=f_speed, tooltip=t_speed, name='Speed ', show=False, smooth_factor=3.0).add_to(fmap)
+
+geojson_data_track = geojson.FeatureCollection(features_tracks)
+folium.GeoJson(geojson_data_track, style_function=f_track, name='Track only', show=True, smooth_factor=3.0).add_to(fmap)
+
+
+folium.LayerControl(collapsed=False).add_to(fmap)
+
+print('saving map ')
+
+# save map to html file
+fmap.fit_bounds(fmap.get_bounds())
+
+#html_file = os.path.join(dir_work, 'index.html')
+html_file = 'index.html'
+if os.path.isfile(html_file):
+    os.system('rm -f '+html_file)
+fmap.save(html_file)
+
+print('opening map in browser ')
+# open html file in default browser
+webbrowser.open(html_file, new=2, autoraise=True)
+
+
+
+
--- process_gpx_files_to_geojson.py
+++ process_gpx_files_to_geojson.py
@@ -1,18 +1,6 @@
 
-# should work from cli
-# all args in cli
-# environment.yml should work
-# functional .gitignore 
-# functional README.md 
-# myroutemap
-
-# ./process_gpx_files_to_geojson.py --data_input_gpx=data_input_gpx --data_processed_gpx=data_processed_gpx' --data_geojson='data_geojson --visualize=False --SI-units=True
-
-# imports
-#import gpxpy
-#import geojson
-#import folium
-#import webbrowser
+# usage
+# python process_gpx_files_to_geojson.py --data_input_raw=data_input_raw --data_processed_gpx=data_processed_gpx --data_geojson=data_geojson
 
 import os
 import numpy as np
@@ -40,34 +28,37 @@ from utils import RDP
 
 #manual_debug = True
 manual_debug = False
-
-data_input_gpx = 'data_input_gpx' 
-data_processed_gpx = 'data_processed_gpx' 
-data_geojson = 'data_geojson'
-
-# command line parameters
-parser = argparse.ArgumentParser(description = 'Extract track, elevation, and, speed from Strava GPX files, export to GeoJSON files and visualize in browser', epilog = 'Report issues to https://github.com/remisalmon/Strava-to-GeoJSON')
-parser.add_argument('--data_input_gpx',     type=str, default='data_input_gpx', help='input .gpx file')
-parser.add_argument('--data_processed_gpx', type=str, default='data_processed_gpx', help = 'output .geojson file')
-parser.add_argument('--data_geojson',       type=str, default='data_geojson', help = 'output .geojson file')
-
-args = parser.parse_args()
-
-main(args)
-
-
-
+if (manual_debug):
+    #dir_work = '/home/craigmatthewsmith/gps_tracks'
+    #os.chdir(dir_work)
+    data_input_raw     = 'data_input_gpx' 
+    data_processed_gpx = 'data_processed_gpx' 
+    data_geojson       = 'data_geojson'
+else: # parse command line parameters
+    parser = argparse.ArgumentParser(description = 'process gpx files to geojson')
+    parser.add_argument('--data_input_raw',     type=str, default='data_input_raw',     help='input .gpx file')
+    parser.add_argument('--data_processed_gpx', type=str, default='data_processed_gpx', help = 'output .geojson file')
+    parser.add_argument('--data_geojson',       type=str, default='data_geojson',       help = 'output .geojson file')
+    args = parser.parse_args()    
+    data_input_raw     = args.data_input_raw
+    data_processed_gpx = args.data_processed_gpx
+    data_geojson       = args.data_geojson 
 
 
+ingest_file_list = glob.glob(os.path.join(data_input_raw, '*.gpx'))
+n_files = len(ingest_file_list)
+print('found %s files to process ' %(n_files))                              
 
+f = 0
+#for f in range(0, 4, 1):
+for f in range(0, n_files, 1):
+    print('  processing %s of %s n_files ' %(f, n_files))                              
+    gpx_file_temp = ingest_file_list[f]
 
-def gpx2geojson(gpx_file_temp, data_input_gpx, data_processed_gpx, data_geojson, use_SI=False, use_RDP=False):
-     
-    # initialize lists
-    lat_lon_data   = []
+    lat_lon_data = []
     elevation_data = []
+    dt_data = []
     timestamp_data = []
-    dt_data        = []
 
     # read GPX file
     with open(gpx_file_temp, 'r') as file:
@@ -83,10 +74,9 @@ def gpx2geojson(gpx_file_temp, data_input_gpx, data_processed_gpx, data_geojson,
     lat_lon_data   = np.array(lat_lon_data)  # [deg, deg]
     elevation_data = np.array(elevation_data) # [m]
     timestamp_data = np.array(timestamp_data) # [s]
-    dt_data = np.array(dt_data) 
-    
+
     n_points = len(timestamp_data)
-    print('  read %s points ' %(n_points))    
+    print('    read %s points ' %(n_points))    
     # calculate trackpoints distance, slope, speed and power
     distance_data = np.zeros([n_points]) # [m]
     slope_data    = np.zeros([n_points]) # [%]
@@ -106,106 +96,47 @@ def gpx2geojson(gpx_file_temp, data_input_gpx, data_processed_gpx, data_geojson,
     slope_data = medfilt(slope_data, 5)
     speed_data = medfilt(speed_data, 5)
 
+    #n_points = len(slope_data)
+    #print('    read %s points ' %(n_points))    
+    
+    use_RDP = True
     # use Ramer–Douglas–Peucker algorithm to reduce the number of trackpoints
-    if (use_RDP):
+    if use_RDP:
         epsilon = 1 # [m]
-        tmp = np.hstack((lat_lon_data, np.arange(0, n_points).reshape((-1, 1)))) # hack
+        tmp = np.hstack((lat_lon_data, np.arange(0, lat_lon_data.shape[0]).reshape((-1, 1)))) # hack
         tmp_new = RDP(tmp, epsilon) # remove trackpoints less than epsilon meters away from the new track
         index = tmp_new[:, 2].astype(int) # hack
-        lat_lon_data   = lat_lon_data  [index,:]
+        lat_lon_data = lat_lon_data[index, :]
         elevation_data = elevation_data[index]
         timestamp_data = timestamp_data[index]
-        distance_data  = distance_data [index]
-        slope_data     = slope_data    [index]
-        speed_data     = speed_data    [index]
-
-
-    n_points = len(slope_data)
-    print('  read %s points ' %(n_points))    
-
-    # convert units
-    if use_SI:
-        speed_data = speed_data*3.6 # m/s to km/h
-    else:
-        speed_data = speed_data*2.236936 # m/s to mph
-
+        distance_data = distance_data[index]
+        slope_data = slope_data[index]
+        speed_data = speed_data[index]
     slope_data = abs(slope_data*100) # decimal to %
 
+    n_points = len(slope_data)
+    print('    read %s points ' %(n_points))    
+    
     # create GeoJSON feature collection
     features = []
     for i in np.arange(1, n_points):
-        #print('    processing %s of %s points ' %(i, n_points))    
-        #print('    %s, %s, %s, %s ' %(lat_lon_data[i-1, 1], lat_lon_data[i-1, 0], lat_lon_data[i, 1], lat_lon_data[i, 0]))    
-        try:
-            line = geojson.LineString([(lat_lon_data[i-1, 1], lat_lon_data[i-1, 0]), (lat_lon_data[i, 1], lat_lon_data[i, 0])]) # (lon,lat) to (lon,lat) format
-            feature = geojson.Feature(geometry=line, properties = {'elevation': float('%.1f'%elevation_data[i]), 'slope': float('%.1f'%slope_data[i]), 'speed': float('%.1f'%speed_data[i])})
-            features.append(feature)
-        except:
-            print('    ERROR %s of %s points ' %(i, n_points))    
-            
+        line = geojson.LineString([(lat_lon_data[i-1, 1], lat_lon_data[i-1, 0]), (lat_lon_data[i, 1], lat_lon_data[i, 0])]) # (lon,lat) to (lon,lat) format
+        feature = geojson.Feature(geometry=line, properties={'elevation': float('%.1f'%elevation_data[i]), 'slope': float('%.1f'%slope_data[i]), 'speed': float('%.1f'%speed_data[i])})
+        features.append(feature)
+
     feature_collection = geojson.FeatureCollection(features)
 
     file_name = os.path.basename(gpx_file_temp.strip('.gpx'))
-
     # write geojson file
-    geojson_write_file = gpx_file_temp.replace(file_name, dt_data[0].strftime('%Y-%m-%d_%H-%M')).replace(data_input_gpx,data_geojson).replace('.gpx','.geojson')        
-    print('  geojson_write_file is %s ' %(geojson_write_file))
+    geojson_write_file = gpx_file_temp.replace(file_name,dt_data[0].strftime('%Y-%m-%d_%H-%M')).replace(data_input_raw,data_geojson).replace('.gpx','.geojson')        
+    print('    geojson_write_file is %s ' %(geojson_write_file))
     with open(geojson_write_file, 'w') as file:
         geojson.dump(feature_collection, file)
-        
+
     # rename and archive gpx file 
-    gpx_file_name_archive = gpx_file_temp.replace(file_name, dt_data[0].strftime('%Y-%m-%d_%H-%M')).replace(data_input_gpx,data_processed_gpx)        
-    print('  gpx_file_name_archive is %s ' %(gpx_file_name_archive))
+    gpx_file_name_archive = gpx_file_temp.replace(file_name,dt_data[0].strftime('%Y-%m-%d_%H-%M')).replace(data_input_raw,data_processed_gpx)        
+    print('    gpx_file_name_archive is %s ' %(gpx_file_name_archive))
     temp_command = 'mv -f '+gpx_file_temp+' '+gpx_file_name_archive
-    print('  temp_command is %s ' %(temp_command))    
-    return
-
-
-def main(args): # main script
-    # parse arguments
-    data_input_gpx     = args.data_input_gpx
-    data_processed_gpx = args.data_processed_gpx
-    data_geojson       = args.data_geojson
-    visualize          = args.visualize # bool
-    SI = True
-    
-    #geojson_filename = args.geojsonfile # str
-    #visualize = args.visualize # bool
-    #SI = args.SI # bool
-
-    print('data_input_gpx is %s ' %(data_input_gpx))
-    gpx_files_to_process = glob.glob(os.path.join(data_input_gpx, '*gpx'))
-    n_gpx_files_to_process = len(gpx_files_to_process)
-    if (n_gpx_files_to_process == 0):
-        print('ERROR - no gpx files to process ')
-        quit()
-    else: 
-        print('found %s gpx files to process' %(n_gpx_files_to_process))
-        
-
-    #if not geojson_filename[-8:] == '.geojson' and not geojson_filename == '':
-    #    print('ERROR: --output is not a GeoJSON file')
-    #    quit()
- 
-    # get GPX and GeoJSON filenames
-    #gpx_file = glob.glob(gpx_filename)[0] # read only 1 file
-    #if not gpx_file:
-    #    print('ERROR: no GPX file found')
-    #    quit()
-
-    #geojson_file = geojson_filename if geojson_filename else gpx_file[:-4]+'.geojson' # use GPX filename if not specified
-
-    # write GeoJSON file
-    f = 0
-    gpx_file_temp = gpx_files_to_process[f]
-    for f, gpx_file_temp in enumerate(gpx_files_to_process):
-        print('  processing f %s of %s, %s ' %(f, n_gpx_files_to_process, gpx_file_temp))
-        gpx2geojson(gpx_file_temp, data_input_gpx, data_processed_gpx, data_geojson, use_SI=SI, use_RDP=visualize)
-
-    # visualize GeoJSON file with Folium
-    #if visualize:
-    #    geojson2folium(geojson_file, use_SI = SI)
-
-
-
+    print('    temp_command is %s ' %(temp_command))    
+    os.system(temp_command)
 
